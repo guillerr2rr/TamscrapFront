b900d7756318e4883003ad8cac70ffb6e81c52c6
guillerr2rr <guillerr2rr@gmail.com>
2024-07-24 02:42:07 +0200

    Inicio

---
+++ Tamscrap/src/main/java/com/Tamscrap/Tamscrap/TamscrapApplication.java
@@ -0,0 +1,13 @@
+package com.Tamscrap.Tamscrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamscrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamscrapApplication.class, args);
+	}
+
+}
